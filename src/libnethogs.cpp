0c2d2de7173ede2bec3a046df24d5107acfab69e
jaemilton <jaemilton@yahoo.com.br>
2021-01-17 21:36:34 -0300

    ajustes para exibir o buffer timeout

--- src/libnethogs.cpp
+++ src/libnethogs.cpp
@@ -103,6 +103,8 @@ static int nethogsmonitor_init(int devc, char **devicenames, bool all,
     }
 
     char errbuf[PCAP_ERRBUF_SIZE];
+    fprintf(stdout, "Packet buffer timeout set to %s\n", to_ms);
+    
     dp_handle *newhandle = dp_open_live(current_dev->name, BUFSIZ, promiscuous,
                                         to_ms, filter, errbuf);
     if (newhandle != NULL) {
